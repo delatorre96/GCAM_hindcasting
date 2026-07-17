bbab3821e3f423506c8a2a50a7b6ac48bd3852f8
delatorre96 <delatorre960@gmail.com>
2026-07-17 13:18:30 +0200

    workflow

--- 4_SensitivityAnalysis/2_2_WorkflowB.R
+++ 4_SensitivityAnalysis/2_2_WorkflowB.R
@@ -43,91 +43,104 @@ if (!file.exists('df_logits.csv')) {
 
 change_config(df_logits, exe_dir, config_file)
 
-n_iterations <- 100
-relative_uncertainty = 0.3
+n_iterations <- 50
+#relative_uncertainty = 0.3
 plan(multisession, workers = 16)
 
 
-timings <- data.frame(
+
+simulation_log <- data.frame(
   iteration = integer(),
+  factor = numeric(),
   time_changing_xml = numeric(),
   time_gcam = numeric(),
-  total_time = numeric()
+  total_time = numeric(),
+  relative_uncertainty = numeric()
 )
 
-for (i in 1:n_iterations){
-  message(paste0('#################################### ITERATION ', i,' ####################################'))
-  message('Inducing uncertainty in the parameters...')
-  t1 <- Sys.time()
-  factor <- runif(
-    1,
-    1 - relative_uncertainty,
-    1 + relative_uncertainty
-  )
-  
-  df_logits_copy <- df_logits
-  df_logits_copy$year  <- 2021
-  df_logits_copy$logit <- round(df_logits_copy$logit * factor, 2)
-  
-  df_split <- split(df_logits_copy, df_logits_copy$xml_file)
-  
-  resultado <- future_lapply(names(df_split), function(xml_file){
-    
-    tabla_logits <- df_split[[xml_file]]
-    
-    xml_file_path <- file.path(dir_xml, xml_file)
-    xml_file_cal  <- file.path(dir_xml, tabla_logits$destination_file[1])
-    
-    tabla_logits$xml_file <- NULL
-    tabla_logits$destination_file <- NULL
-    
-    insertar_logits(
-      xml_file_path,
-      tabla_logits,
-      xml_file_cal
+for (relative_uncertainty in c(0.2, 0.3, 0.5, 0.6)){
+  for (i in 1:n_iterations){
+    message(paste0('#################################### ITERATION ', i,' ####################################'))
+    message('Inducing uncertainty in the parameters...')
+    t1 <- Sys.time()
+    factor <- runif(
+      1,
+      1 - relative_uncertainty,
+      1 + relative_uncertainty
     )
     
-    tabla_logits
-  })
+    df_logits_copy <- df_logits
+    df_logits_copy$year  <- 2021
+    df_logits_copy$logit <- round(df_logits_copy$logit * factor, 2)
+    
+    df_split <- split(df_logits_copy, df_logits_copy$xml_file)
+    
+    resultado <- future_lapply(names(df_split), function(xml_file){
+      
+      tabla_logits <- df_split[[xml_file]]
+      
+      xml_file_path <- file.path(dir_xml, xml_file)
+      xml_file_cal  <- file.path(dir_xml, tabla_logits$destination_file[1])
+      
+      tabla_logits$xml_file <- NULL
+      tabla_logits$destination_file <- NULL
+      
+      insertar_logits(
+        xml_file_path,
+        tabla_logits,
+        xml_file_cal
+      )
+      
+      tabla_logits
+    })
+    
+    df_logits_with_uncer <-
+      bind_rows(resultado) |>
+      select(region, supplysector, subsector, logit)
+    t2 <- Sys.time()
+    
+    run_gcam(run_gcam_file_cal)
+    
+    message('Saving results....')
   
-  df_logits_with_uncer <-
-    bind_rows(resultado) |>
-    select(region, supplysector, subsector, logit)
-  t2 <- Sys.time()
-  
-  run_gcam(run_gcam_file_cal)
-  
-  message('Saving results....')
-
-  
-  ok <- tryCatch(
-    {
-      append_iteration_results()
-      TRUE
-    },
-    error = function(e) {
-      message(e$message)
-      FALSE
+    
+    ok <- tryCatch(
+      {
+        append_iteration_results()
+        TRUE
+      },
+      error = function(e) {
+        message(e$message)
+        FALSE
+      }
+    )
+    
+    if (ok) {
+      append_input(df_logits_with_uncer, file.path("Data", "inputs",'df_logits.csv'))
     }
-  )
-  
-  if (ok) {
-    append_input(df_logits_with_uncer, file.path("Data", "inputs",'df_logits.csv'))
-  }
-  
-  delete_iteration_csvs()
-  t3 <- Sys.time()
-  
-  timings <- rbind(
-    timings,
-    data.frame(
-      iteration = i,
-      time_changing_xml = as.numeric(t2 - t1, units = "mins"),
-      time_gcam = as.numeric(t3 - t2, units = "mins"),
-      total_time = as.numeric(t3 - t1, units = "mins")
+    
+    delete_iteration_csvs()
+    t3 <- Sys.time()
+    
+    simulation_log <- rbind(
+      simulation_log,
+      data.frame(
+        iteration = i,
+        factor = factor,
+        relative_uncertainty,
+        time_changing_xml = as.numeric(t2 - t1, units = "mins"),
+        time_gcam = as.numeric(t3 - t2, units = "mins"),
+        total_time = as.numeric(t3 - t1, units = "mins")
+      )
     )
-  )
-  write.csv(timings, "timings.csv", row.names = FALSE)
+    
+    write.csv(
+      simulation_log,
+      "simulation_log.csv",
+      row.names = FALSE
+    )
+    
+  }
 }
 
 
@@ -140,67 +153,6 @@ for (i in 1:n_iterations){
 
 
 
-
-
-
-
-# 
-# ############ Extraer las combinaciones únicas de cada data frame
-# combis <- lapply(tablas_logits, \(df) {
-#   unique(df[, c("supplysector", "subsector")])
-# })
-# 
-# # Comparar todos los pares
-# res <- combn(names(combis), 2, simplify = FALSE, FUN = function(par) {
-#   comunes <- merge(
-#     combis[[par[1]]],
-#     combis[[par[2]]],
-#     by = c("supplysector", "subsector")
-#   )
-#   
-#   if (nrow(comunes) > 0) {
-#     data.frame(
-#       df1 = par[1],
-#       df2 = par[2],
-#       n_coincidencias = nrow(comunes),
-#       coincidencias = I(list(comunes))
-#     )
-#   }
-# })
-# 
-# # Mantener solo los pares con coincidencias
-# res <- do.call(rbind, Filter(Negate(is.null), res))
-# 
-# res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-xml_file_cal <- paste0(dir_xml,"/en_transformation_EUR_cal.xml")
-
-tabla_logits <- extraer_logits_anyXML(xml_file)
-tabla_logits$year <- 2021
-tabla_logits$logit <- tabla_logits$logit + rnorm(nrow(tabla_logits),0,0.2)
-insertar_logits(xml_file,tabla_logits, xml_file_cal)
 
 
 
